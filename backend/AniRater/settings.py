6de0ddfdd9eb9ed5443c0045643d10124fd90bc6
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-10-17 16:46:31 +0300

    rig user to installed apps

--- backend/AniRater/settings.py
+++ backend/AniRater/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'game.apps.GameConfig',
+    'user.apps.UserConfig'
     'rest_framework',
     'corsheaders',
 
